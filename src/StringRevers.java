0d829cf85e27fab1752247e80e58266f4d80e5e0
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2023-01-11 17:21:52 -0600

    Initial commit

---
+++ src/StringRevers.java
@@ -0,0 +1,38 @@
+public class StringRevers {
+
+    public static void main(String[] args) {
+
+        String str = "Aloo";
+        System.out.println(reverse(str));
+
+//        String str1 = "123";
+//        System.out.println(reverse(str));
+
+
+    }
+    public static String reverse(String str) {
+        if (str == null) throw new IllegalArgumentException("String can not be empty");
+
+        String revers = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            revers += str.charAt(i);
+
+        }
+        return revers;
+    }
+
+
+//    public static String reverse(String in) {
+//        if (in == null)
+//            throw new IllegalArgumentException("Null is not valid input");
+//
+//        StringBuilder out = new StringBuilder();
+//
+//        char[] chars = in.toCharArray();
+//
+//        for (int i = chars.length - 1; i >= 0; i--)
+//            out.append(chars[i]);
+//
+//        return out.toString();
+//    }
+}
