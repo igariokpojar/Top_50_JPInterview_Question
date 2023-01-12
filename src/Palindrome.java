8dc51bb63746eac066789a0801db5e44205941cd
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2023-01-11 18:58:14 -0600

    Self Practice

---
+++ src/Palindrome.java
@@ -0,0 +1,44 @@
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("Alex")); //false
+        System.out.println(isPalindrome("Alla")); // true
+        System.out.println(checkPalindrome("Ava")); // true
+
+    }
+    public static boolean isPalindrome(String str){
+
+        String revers = "";
+        str = str.toLowerCase();
+        for (int i = str.length()-1; i >=0 ; i--) {
+            revers+=str.charAt(i);
+        }
+        if (revers.equals(str)){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkPalindrome(String input) {
+        boolean result = true;
+        int length = input.length();
+        input=input.toLowerCase();
+
+        for (int i = 0; i < length/2; i++) {
+            if (input.charAt(i) != input.charAt(length - i - 1)) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+}
+/*
+A palindrome string is the same string backwards or forwards.
+To check for a palindrome, you can reverse the input string and check if the result
+ is equal to the input. The following example code shows how to use
+ the String charAt(int index) method to check for palindrome strings:
+ */
