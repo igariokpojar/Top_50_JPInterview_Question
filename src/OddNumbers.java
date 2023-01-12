b73e348293af4b98d5de40596aec67391f90d4fb
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2023-01-11 18:35:53 -0600

    Self Practice

---
+++ src/OddNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class OddNumbers {
+
+    public static void main(String[] args) {
+        System.out.println(isOdd(Arrays.asList(2)));
+
+    }
+    public static boolean isOdd(List<Integer> num){
+        for (int each:num){
+            if (each%2!=0)return true;
+        }
+        return false;
+    }
+}
