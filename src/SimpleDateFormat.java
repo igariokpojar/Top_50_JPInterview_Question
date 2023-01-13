a254c448a085c6d4fddab90786024eaced35958d
Igor Pojar <104471774+igariokpojar@users.noreply.github.com>
2023-01-13 09:16:01 -0600

    Self Practice

---
+++ src/SimpleDateFormat.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SimpleDateFormat {
+
+    public static void main(String[] args) {
+
+//        String pattern = "MM-dd-yyyy";
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+//
+//        String date = simpleDateFormat.format(new Date());
+//        System.out.println(date); // 06-23-2020
+
+  }
+}
